844c0318443fae9c9a1b5ce5f2298bd4c14d0c1e
tjphilippe <tjphilippe@ufl.edu>
2017-02-14 21:25:46 -0500

    Directory Model
    +Directory Owner
    +Directory Name
    +Date Created
    +Parent Directory

--- srs/models.py
+++ srs/models.py
@@ -49,3 +49,17 @@ class Notecard(models.Model):
 
     def __str__(self):
         return self.name
+
+class Directories(models.Model):
+    author = models.ForeignKey('auth.User')
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    parent_directory = models.ForeignKey('self',related_name='child_directory', null=True, on_delete=models.CASCADE)
+
+    def create(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
