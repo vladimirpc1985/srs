87aa69465c7f03b054b2eddf12001c6062edfade
tjphilippe <tjphilippe@ufl.edu>
2017-04-12 23:23:13 -0400

    Shift_updates
    +Cleaned notecard_list template file
    +Arrow keys will not affect window scrollbar
    +add shifting capabilities (needs troubleshooting)

--- srs/static/js/notecard_navigate.js
+++ srs/static/js/notecard_navigate.js
@@ -1,13 +1,25 @@
 $(document).keydown(
     function(e)
     {
-    	if (e.keyCode == 38) { //Up
+    	if (e.keyCode == 40 ) { //Up
 			$(".imageDiv:focus").next().focus();
-            //alert(notecards);
+            if(index > notecard_top ){
+                notecard_top += 1;
+                start = notecard_top - 5
+                new_list = trim_list(notecards, start);
+                load_notecards(new_list);
+                index++; 
+            }
+             
         }
-        if (e.keyCode == 40) { //Down
+        if (e.keyCode == 38) { //Down
             $(".imageDiv:focus").prev().focus();
-            //alert(notecards);
+                index--;
+                if(index < notecard_bottom){
+                notecard_bottom -= 1;
+                new_list = trim_list(notecards, notecard_bottom);
+                load_notecards(new_list);
+            }
         }
     }
 );
@@ -15,18 +27,24 @@ $(document).keydown(
 
 $(document).ready(function() {
     document.getElementById('bottomDiv1').focus();
-    //LOAD CONTENT INTO DIV
-    //alert(notecards);
-    //alert(Object.keys(notecards).length);
-    for(var i = 0; i < 1; i++) {
-        var id =  'bottomDiv'+ i;
-       // alert(notecards[1]);
-        document.getElementById("bottomDiv1").innerHTML = notecards[1].name + "<br>" + notecards[1].label;
-        //alert(notecards[i].model);
-
-    //var obj = json[i];
-
-    //console.log(obj.id);
-}
-}	
+    load_notecards(notecards);
+}   
 );
+
+function load_notecards(notecards){
+     for(var i = 0; i < 5; i++) {
+        var id =  'bottomDiv'+ (i+1);
+        document.getElementById(id).innerHTML = notecards[i].fields.name + "<br>" + notecards[i].fields.body;
+        document.getElementById(id).href = 'http://'+window.location.host+'/notecard/'+notecards[i].pk+'/';
+}
+}
+
+function trim_list(notecards, index){
+
+    new_list = [];
+    var end = index + 5;
+    for(index; index < end; index++){
+        new_list.push(notecards[index]);
+    }
+    return new_list;
+}
